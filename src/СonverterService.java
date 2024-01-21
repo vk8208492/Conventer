474de65c90e6b0ff3bfd6269aa313b1475538c8c
Vlad <147541054+kvs23031999@users.noreply.github.com>
2024-01-21 18:17:27 +0200

    initial commit

--- src/СonverterService.java
+++ src/СonverterService.java
@@ -3,10 +3,12 @@ public class СonverterService {
     private final double KM_IN_MILE = 1.852;
 
     public double convertKmToMiles(double km) {
+
         return km / KM_IN_MILE;
     }
     public double convertMileToKm(double mile){
-        return mile*KM_IN_MILE;
+
+        return mile * KM_IN_MILE;
     }
 
 }
